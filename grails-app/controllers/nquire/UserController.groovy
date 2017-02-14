1c4ce7bd5acb60db528b2ddeac641eac28d66d6b
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-14 11:57:56 +0100

    Added register logic (doesn't work currently)

--- grails-app/controllers/nquire/UserController.groovy
+++ grails-app/controllers/nquire/UserController.groovy
@@ -1,4 +1,7 @@
-package nquire
+package nquire;
+
+import nquire.User;
+import nquire.Role;
 
 class UserController {
 
@@ -9,6 +12,23 @@ class UserController {
     }
 
     def save() {
+        def firstName = params.string('firstName');
+        def lastName = params.string('lastName');
+        def email = params.string('email');
+        def username = params.string('username');
+        def password = params.string('password');
 
+        if(firstName == "" || lastName == "" || email == "" || username == "" || password.length < 6) {
+            // Invalid request
+            response.sendError(400);
+            return;
+        }
+
+        def user = new User(username: username, firstName: firstName, lastName: lastName,
+                            email: email, password: password).save();
+
+        UserRole.create(user, Role.findByAuthority('ROLE_LECTURER'));
+
+        redirect(controller: 'login');
     }
 }
