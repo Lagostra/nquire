c7eb0c2297864138516e3e1bad39a4b2082ae85a
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-17 13:23:12 +0100

    Bootstrap.groovy cleanup

--- grails-app/init/nquire/BootStrap.groovy
+++ grails-app/init/nquire/BootStrap.groovy
@@ -8,11 +8,6 @@ class BootStrap {
 
     def init = { servletContext ->
         Role lecturerRole = new Role(authority: 'ROLE_LECTURER').save(failOnError: true)
-
-        UserRole.withSession {
-            it.flush()
-            it.clear()
-        }
     }
     def destroy = {
     }
