91f05217e273d5d604088203aebf2cf078b8792d
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-19 14:33:18 +0100

    Added FileController with upload and list functionality

---
+++ grails-app/controllers/nquire/FileController.groovy
@@ -0,0 +1,63 @@
+package nquire
+
+import grails.plugin.springsecurity.annotation.Secured;
+import java.util.UUID;
+
+import nquire.Presentation;
+import nquire.User;
+
+@Secured('ROLE_LECTURER')
+class FileController {
+
+    static allowedMethods = [index: 'GET', upload: 'GET', save: 'POST', show: 'GET'];
+
+    def index() {
+        render(view: 'list', model: [presentations: authenticatedUser.presentations]);
+    }
+
+    def upload() {
+        render(view: 'upload');
+    }
+
+    def save() {
+        def f = request.getFile('file');
+        String title = params.title;
+
+        if(f.empty) {
+            flash.message = 'File cannot be empty';
+            render(view: 'upload');
+            return;
+        }
+
+        String filename = UUID.randomUUID().toString();
+
+        //f.transferTo(new File("D:\\Eivind\\workspaces\\Java2\\presentations\\test.pdf"));
+        f.transferTo(new File(grailsApplication.config.getProperty('presentationRoot') + filename + ".pdf"));
+        println grailsApplication.config.getProperty('presentationRoot');
+
+        User user = authenticatedUser;
+        Presentation pres = new Presentation(title: title, fileName: filename, owner: user).save(flush: true, failOnError: true);
+
+        redirect(action: 'index');
+    }
+
+    def show() {
+        Presentation pres = Presentation.findById(params.id);
+
+        if(pres == null) {
+            // Presentation does not exist
+            request.sendError(404);
+            return;
+        }
+
+        if(pres.owner != authenticatedUser) {
+            // Presentation is not owned by signed in user
+            request.sendError(403);
+            return;
+        }
+
+        render(file: new File(grailsApplication.config.getProperty('presentationRoot') + pres.fileName + ".pdf"),
+                fileName: pres.title + ".pdf");
+    }
+
+}
