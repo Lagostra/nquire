3167b64011e5731982b8b401c1ad097b42fe31aa
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-19 14:32:33 +0100

    Changed domain class Presentation and User: added relation

--- grails-app/domain/nquire/Presentation.groovy
+++ grails-app/domain/nquire/Presentation.groovy
@@ -1,15 +1,27 @@
 package nquire
 
+import nquire.User;
+
 class Presentation {
 
     String title;
     String fileName;
-    int owner;
+
+    static belongsTo = [owner: User]
+
+    static mapping = {
+        owner column: 'Owner_Id'
+    }
 
     static constraints = {
-        title size: 5..100, blank: false
+        title size: 3..100, blank: false
         fileName blank: false
-        owner blank: false
+    }
+
+    def beforeDelete() {
+        // Delete the file that this object references
+        File file = new File(grailsApplication.config.getProperty('presentationRoot') + filename + ".pdf");
+        file.delete();
     }
 
 }
