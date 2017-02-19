c0f43da8dd063f0ebddec64d853b7b2c2d357aab
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-19 12:32:56 +0100

    Added test user when in development environment

--- grails-app/init/nquire/BootStrap.groovy
+++ grails-app/init/nquire/BootStrap.groovy
@@ -4,11 +4,27 @@ import nquire.User;
 import nquire.Role;
 import nquire.UserRole;
 
+import grails.util.Environment;
+
 class BootStrap {
 
     def init = { servletContext ->
+        // Create lecturer role if it does not exist in database
         if(Role.find(new Role(authority: 'ROLE_LECTURER')) == null)
             Role lecturerRole = new Role(authority: 'ROLE_LECTURER').save(failOnError: true)
+
+        if(Environment.current == Environment.DEVELOPMENT) {
+            // Create test user if in development environment
+            def testUser = new User(username: 'test', password: 'password', firstName: "Test",
+                                    lastName: "Testsson", email: "test@test.com").save()
+
+            UserRole.create(testUser, adminRole);
+
+            UserRole.withSession {
+                it.flush();
+                it.clear();
+            }
+        }
     }
     def destroy = {
     }
