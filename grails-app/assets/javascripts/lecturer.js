966e70872d640faba1a282f90ead87737ef82b23
Eivind Lie Andreassen <ela4991@gmail.com>
2017-02-23 11:11:09 +0100

    Added javascript for lecturer present view

---
+++ grails-app/assets/javascripts/lecturer.js
@@ -0,0 +1,29 @@
+var socket;
+
+function init() {
+    socket = new WebSocket(url);
+    console.log("Connecting...")
+
+    socket.onopen = function(e) {
+        console.log("Connection established");
+        var msg = { "type": "connect",
+            "lectureId": lectureId,
+            "role": "lecturer",
+            "token": token};
+        socket.send()
+    }
+
+    socket.onmessage = function(e) {
+        console.log(e.data);
+
+        setTimeout(function() {socket.send("Test")}, 5000);
+    }
+
+    socket.onerror = function(e) {
+
+    }
+
+    socket.onclose = function(e) {
+        console.log("Connection closed.");
+    }
+}
