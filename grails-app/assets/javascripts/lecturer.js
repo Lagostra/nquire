487e6b61c5f7d407f2c296eb21a732a218c46b3e
lars møster <lars.moster@hotmail.com>
2017-02-27 17:59:59 +0100

    documentation lecturer.js

--- grails-app/assets/javascripts/lecturer.js
+++ grails-app/assets/javascripts/lecturer.js
@@ -4,7 +4,7 @@
 
 //list of strings
 var questions = [];
-var question_conatiner = document.getElementById('question_container');
+var question_container = document.getElementById('question_container');
 
 
 /*call this function upon event, when new question is received*/
@@ -16,14 +16,14 @@ var addQuestion = function(question) {
 
 //show/hide questions on lecturer screen (on/off)
 var displayQuestions = function (on) {
-    if (!has_class(question_conatiner, hidden)) {
+    if (!has_class(question_container, hidden)) {
         on ? add_class(question_container, hidden)
-            : remove_class(question_conainer, hidden);
+            : remove_class(question_container, hidden);
     }
 }
 
 //notify the lecturer of a new question
-var notifyNewQestion = function () {
+var notifyNewQuestion = function () {
 
 }
 
@@ -44,5 +44,6 @@ var add_class = function(element, cls) {
 
 //remove class from element safely
 var remove_class = function(element, cls) {
-    element.className = e.className.replace( /(?:^|\s)gray_out(?!\S)/g , '' );
+    element.className = e.className.replace( /(?:^|\s)cls(?!\S)/g , '' );
 }
+/* TODO: FIX DDENNE REGEXEN ^ */
