2e0819a63a6d66779e21119558d1a9e0366f16cb
lars møster <lars.moster@hotmail.com>
2017-02-27 16:52:42 +0100

    opprettet LecturerShowQuestions.js

---
+++ grails-app/assets/javascripts/LecturerShowQuestions.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lars on 27.02.2017.
+ */
+
+/*list of strings*/
+var questions = [];
+
+var main = function(){
+
+}
+
+/*From Lecturer.js call this function on event*/
+var addQuestion = function(question) {
+    questions.append(question);
+}
+
+var
+
+
