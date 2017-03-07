0e8633c82e4defca83d56a4ae48200351226a857
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-07 15:40:53 +0100

    questions now uses bootstrap modal

--- grails-app/assets/javascripts/lecturer.js
+++ grails-app/assets/javascripts/lecturer.js
@@ -8,7 +8,6 @@ var questions = [];
 var question_container;
 var default_question;
 var display_question_btn;
-var hide_question_btn;
 var new_question_badge;
 
 var class_hidden = "hidden";
@@ -17,172 +16,18 @@ var class_new_question = "new_question";
 
 var socket;
 
-
-//Initialize variables, set events and declare socket related methods
 function initLecturer() {
     question_container = document.getElementById("question_container");
     default_question = document.getElementById("default_question");
     display_question_btn = document.getElementById("display_question_btn");
-    hide_question_btn = document.getElementById("hide_question_btn");
     new_question_badge = document.getElementById("new_question_badge");
 
     /* EVENTS */
     display_question_btn.onclick = function () {
-        questionsSetHidden(false);
-    };
-    hide_question_btn.onclick = function () {
-        questionsSetHidden(true);
-    };
-
-    /* SOCETS */
-    socket = new WebSocket(url);
-    console.log("Connecting...");
-
-    socket.onopen = function(e) {
-        console.log("Connection established");
-        var msg = { "type": "connect",
-            "lectureId": lectureId,
-            "role": "lecturer",
-            "token": token};
-        socket.send(JSON.stringify(msg))
-    };
-
-    socket.onmessage = function(e) {
-        var msg = JSON.parse(e.data);
-
-        switch(msg.type) {
-            case "connected": // Successfully connected to lecture
-                socket.send(JSON.stringify({"type": "requestPresentation"}));
-                break;
-            case "presentation": // Received presentation file
-                setPresentation(presentation);
-                break;
-        }
-    };
-
-    socket.onerror = function(e) {
-
-    };
-
-    socket.onclose = function(e) {
-        console.log("Connection closed.");
-    };
-
-    window.onkeydown = onKey;
-}
-
-//Call this function when new questions are received, adds question and HTML
-var addQuestion = function(question) {
-    question.new = true;
-    questions.push(question);
-    notifyNewQuestion();
-    default_question.classList.add(class_hidden);
-    question_container.innerHTML +=
-        '<div class="row">' +
-            '<div class="col-md-2"></div>' +
-            '<div class="col-md-8">' +
-                '<div id ="' + questions.length + '" ' +
-                    'class="question ' +
-                    class_new_question + ' ">' +
-                    question +
-                ' </div> ' +
-            '</div> ' +
-        '</div>';
-};
-
-//Notify the lecturer of a new question
-var notifyNewQuestion = function () {
-    // Hvis questions er displayed skal ikke knappen få "new" taggen
-    if (getQuestionsToggled()) {return}
-
-    display_question_btn.classList.add(class_new_btn);
-    new_question_badge.innerHTML = getNewQuestions();
-    new_question_badge.classList.remove(class_hidden);
-};
-
-//Check whether questions are displayed
-var getQuestionsToggled = function() {
-    return question_container.classList.contains(class_hidden);
-};
-
-//Show/hide questions on lecturer screen
-var questionsSetHidden = function (hidden) {
-    if (hidden) {
-        question_container.classList.add(class_hidden);
         question_container.scrollTop = question_container.scrollHeight;
-        resetNewQuestions();
-    }
-    else {
-        question_container.classList.remove(class_hidden);
-        display_question_btn.classList.remove(class_new_btn);
-    }
-};
-
-//Remove the new_question class from all question elements
-var resetNewQuestions = function () {
-    var new_questions = document.getElementsByClassName("question");
-    for (var i = 0; i < new_questions.length; i++){
-        new_questions[i].classList.remove(class_new_question);
-    }
-    new_question_badge.innerHTML = getNewQuestions();
-    new_question_badge.classList.add(class_hidden);
-};
-
-//Reset the questions array,
-var clearAllQuestions = function () {
-    questions = [];
-    setDefaultQuestion();
-};
-
-//Adds the default question "no questions yet"
-var setDefaultQuestion = function () {
-    question_container.innerHTML =
-        '<div id="question_container" class="hidden">' +
-        '<div class="row">' +
-            '<div class="col-md-2"></div>' +
-            '<div class="col-md-8">' +
-                '<div id="default_question" class="question">' +
-                    '<p>No questions yet</p>' +
-                '</div>' +
-            '</div>' +
-        '</div>' +
-    '</div>';
-};
-
-//Get the amount of new questions
-var getNewQuestions = function () {
-    return document.getElementsByClassName(class_new_question).length;
-};
-
-function onKey(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    switch(key) {
-        case 37: // Left
-            renderPreviousPage();
-            socket.send(JSON.stringify({type: "pageChange", page: currentPage}));
-            break;
-        case 39: // Right
-            renderNextPage();
-            socket.send(JSON.stringify({type: "pageChange", page: currentPage}));
-            break;
-    }
-}
-
-function initLecturer() {
-    question_container = document.getElementById("question_container");
-    default_question = document.getElementById("default_question");
-    display_question_btn = document.getElementById("display_question_btn");
-    hide_question_btn = document.getElementById("hide_question_btn");
-
-    /* EVENTS */
-    display_question_btn.onclick = function () {
-        questionsSetHidden(false);
-    };
-    hide_question_btn.onclick = function () {
-        questionsSetHidden(true);
     };
 
-    /* SOCETS */
+    /* SOCkETS */
     socket = new WebSocket(url);
     console.log("Connecting...");
 
@@ -227,4 +72,88 @@ function initLecturer() {
     };
 
     window.onkeydown = onKey;
+
+    $('#questionsModal').on('hidden.bs.modal', function(e) {
+       resetNewQuestions();
+    });
+}
+
+//Call this function when new questions are received, adds question and HTML
+var addQuestion = function(question) {
+    removeDefaultQuestion();
+    question.new = true;
+    questions.push(question);
+    notifyNewQuestion();
+    default_question.classList.add(class_hidden);
+    question_container.innerHTML +=
+        '<div ' +
+            'class="question ' +
+            class_new_question + ' ">' +
+            question +
+        ' </div>';
+};
+
+//Notify the lecturer of a new question
+var notifyNewQuestion = function () {
+    // Hvis questions er displayed skal ikke knappen få "new" taggen
+    if (getQuestionsToggled()) {return}
+
+    display_question_btn.classList.add(class_new_btn);
+    new_question_badge.innerHTML = getNewQuestions();
+    new_question_badge.classList.remove(class_hidden);
+};
+
+//Check whether questions are displayed
+var getQuestionsToggled = function() {
+    return question_container.classList.contains(class_hidden);
+};
+
+//Remove the new_question class from all question elements
+var resetNewQuestions = function () {
+    var new_questions = document.getElementsByClassName("question");
+    for (var i = 0; i < new_questions.length; i++){
+        new_questions[i].classList.remove(class_new_question);
+    }
+    display_question_btn.classList.remove(class_new_btn);
+    new_question_badge.innerHTML = getNewQuestions();
+    new_question_badge.classList.add(class_hidden);
+};
+
+//Reset the questions array,
+var clearAllQuestions = function () {
+    questions = [];
+    setDefaultQuestion();
+};
+
+//Adds the default question "no questions yet"
+var setDefaultQuestion = function () {
+    question_container.innerHTML =
+        '<div id="default_question" class="question">' +
+            '<p>No questions yet</p>' +
+        '</div>';
+};
+
+var removeDefaultQuestion = function() {
+    var default_question = document.getElementById("default_question");
+    if(default_question)
+        default_question.parentElement.removeChild(default_question);
+};
+
+//Get the amount of new questions
+var getNewQuestions = function () {
+    return document.getElementsByClassName(class_new_question).length;
+};
+
+function onKey(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+    switch(key) {
+        case 37: // Left
+            renderPreviousPage();
+            socket.send(JSON.stringify({type: "pageChange", page: currentPage}));
+            break;
+        case 39: // Right
+            renderNextPage();
+            socket.send(JSON.stringify({type: "pageChange", page: currentPage}));
+            break;
+    }
 }
--- grails-app/views/lecture/present.gsp
+++ grails-app/views/lecture/present.gsp
@@ -3,35 +3,44 @@
     <asset:stylesheet src="application.css" />
 </head>
 <body>
+    <div class="modal fade" id="questionsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title">Questions</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div id="question_container">
+                        <div id="default_question" class="question">
+                            <p>No questions yet</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <h1>Lecture ID: <sec:loggedInUserInfo field='currentLecture'/></h1>
     <g:form controller="lecture" action="close_lecture">
         <g:submitButton name="Submit" value="Close lecture" class="btn btn-primary" />
     </g:form>
 
-    <div id="display_question_btn" class="btn btn-primary btn-md">
+    <button id="display_question_btn" class="btn btn-primary btn-md" data-toggle="modal" data-target="#questionsModal" type="button">
         Show Questions <span id="new_question_badge" class="badge"></span>
-    </div>
-    <div id="hide_question_btn" class="btn btn-primary btn-md">
-        Hide Questions
-    </div>
-
-
-    <div id="question_container" class="hidden">
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8">
-                <div id="default_question" class="question">
-                    <p>No questions yet</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    </button>
 
     <div><span >Page: <span id="page_num"></span> / <span id="page_count"></span></span></div>
     <div id="presentation-container" style="z-index: -10;">
         <canvas id="the-canvas"></canvas>
     </div>
 
+    <asset:javascript src="application.js"/>
     <asset:javascript src="pdf.js" />
     <asset:javascript src="pdfRenderer.js"/>
     <asset:javascript src="lecturer.js"/>
