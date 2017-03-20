52a45dd546dd93969fba9677e5b8532e4e24933b
lars møster <lars.moster@hotmail.com>
2017-03-20 17:04:49 +0100

    La til enda litt mer kode i en av filene (neida men serr, jeg gravde ut en nå deprokated metode, og nå er algoritmen raskere for å sjekke ut ting

--- src/main/groovy/nquire/pace/Pace.java
+++ src/main/groovy/nquire/pace/Pace.java
@@ -16,13 +16,12 @@ public class Pace implements PaceInterface {
 
         double base = 50.0;
         double feedback = 0.0;
-        double timeElapsed = getTimeElapsed(feedbackList);
 
         double sizeConstant = 2 - Math.pow(1.014, feedbackList.size());
 
         for (Feedback fb : feedbackList){
-            if (fb.getFast()) feedback += calculateTime(timeElapsed, sizeConstant);
-            else feedback -= calculateTime(timeElapsed, sizeConstant);
+            if (fb.getFast()) feedback += calculateTime(fb.getTimestamp(), sizeConstant);
+            else feedback -= calculateTime(fb.getTimestamp(), sizeConstant);
         }
         return base + feedback;
     }
@@ -43,25 +42,6 @@ public class Pace implements PaceInterface {
                 new Date(System.currentTimeMillis()+5*60*1000).getTime());
         if (a < 0.0 ) return 0;
         return a;
-    }
-
-    private double getTimeElapsed(ArrayList<Feedback> feedbackList) {
-        Date first =  feedbackList.get(0).getTimestamp();
-
-        if (first == null){
-            throw new IllegalStateException
-                    ("No timestamp could be gathered from list");
-        }
-
-        for (Feedback fb : feedbackList){
-            Date time = fb.getTimestamp();
-            if (time.before(first)){
-                first = time;
-            }
-        }
-
-        //returns seconds since the first question was timestamped
-        return (System.currentTimeMillis() - first.getTime()) / 1000;
     }
 
 }
