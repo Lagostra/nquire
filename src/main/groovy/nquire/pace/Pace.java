0352b50b4f5c7145d86d2c233d3c3b0bd298ee78
lars møster <lars.moster@hotmail.com>
2017-03-21 12:01:40 +0100

    endret kalkulasjonene, færre magic numbers, og en bedre funksjon overall + dokumentasjon / lesbarhet

--- src/main/groovy/nquire/pace/Pace.java
+++ src/main/groovy/nquire/pace/Pace.java
@@ -10,28 +10,33 @@ import java.util.Date;
 public class Pace implements PaceInterface {
 
     @Override
-    public double calculateCurrentPace(List<Feedback> feedbackList) {
+    public double calculateCurrentPace(List<Feedback> feedbackList, int numStudents) {
         if (feedbackList == null || feedbackList.isEmpty())
             return 50.0;
 
         double base = 50.0;
         double feedback = 0.0;
 
-        double sizeConstant = 2 - Math.pow(1.014, feedbackList.size());
+        double sizeConstant = calculateSizeConstant(numStudents);
 
         for (Feedback fb : feedbackList){
             if (fb.getFast())
-                feedback += calculateTime(fb.getTimestamp(), sizeConstant);
+                feedback += calculateTime(fb.getTimestamp())
+                        * 50 * sizeConstant;
             else
-                feedback -= calculateTime(fb.getTimestamp(), sizeConstant);
+                feedback -= calculateTime(fb.getTimestamp())
+                        * 50 * sizeConstant;
         }
         return base + feedback;
     }
 
 
-    private double calculateTime(Date timestamp, double sizeConstant) {
+    private double calculateTime(Date timestamp) {
+        double deltaTime = compareTime(timestamp);
+        if (deltaTime > 300) return 0.0;
+        //27 mill er 300^3, altså er feedback gyldig i 5 min
+        double a = 1 - (Math.pow(deltaTime, 3) / 27000000);
 
-        double a = 0.8 * Math.pow(compareTime(timestamp),0.45)* sizeConstant;
         if (a > 1.0) a = 1.0;
         else if (a < -1.0) a = -1.0;
         return a;
@@ -40,11 +45,10 @@ public class Pace implements PaceInterface {
 
     //Hvor lang tid mellom 5 minutter siden og timestamp
     private double compareTime(Date timestamp) {
-        double a = ((timestamp.getTime() -
-                new Date(System.currentTimeMillis()+5*60*1000).getTime()
-                    )/1000);
-        if (a < 0.0 ) return 0.0;
-        return a;
+        return System.currentTimeMillis() - timestamp.getTime();
     }
 
+    double calculateSizeConstant(int numStudents) {
+        return (10/numStudents < 1.0) ? 10/numStudents : 1.0;
+    }
 }
