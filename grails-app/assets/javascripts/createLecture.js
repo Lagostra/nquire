4f4a773d2d76a5d16305fd338f44e84171786848
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-24 22:37:26 +0100

    Added button to remove presentation on create lecture view

--- grails-app/assets/javascripts/createLecture.js
+++ grails-app/assets/javascripts/createLecture.js
@@ -1,5 +1,15 @@
 function setLectureFile(id, source) {
     document.forms['create-lecture-form']['presentationId'].value = id;
-    document.getElementById('presentation-selector').innerHTML = source.innerHTML;
+    document.getElementById('presentation-selector').innerHTML =
+        '<button id="remove-presentation" title="Remove presentation" type="button" class="close">'+'' +
+            '<span>&times;</span>' +
+        '</button>';
+    document.getElementById('presentation-selector').innerHTML += source.innerHTML;
+
     $('#file-modal').modal('hide');
+
+    document.getElementById('remove-presentation').onclick = function(e) {
+        document.forms['create-lecture-form']['presentationId'].value = 0;
+        document.getElementById('presentation-selector').innerHTML = 'Select presentation file';
+    };
 }
