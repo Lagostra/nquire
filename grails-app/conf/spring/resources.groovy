c75d91e2345d6eec1f14684b9d3ed3d3699f97b0
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-27 17:03:22 +0200

    Language is now forced to english

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,4 +3,9 @@ import nquire.MyUserDetailsService;
 // Place your Spring DSL code here
 beans = {
     userDetailsService(MyUserDetailsService);
+
+    localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+        defaultLocale = new Locale("en","GB")
+        java.util.Locale.setDefault(defaultLocale)
+    }
 }
