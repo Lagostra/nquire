0692bef4717a110b8f0550b00985e7a62f48d5e0
lars møster <lars.moster@hotmail.com>
2017-03-28 12:22:03 +0200

    added test for lecturer, will be updated

--- grails-app/assets/javascripts/spec/lecturerSpec.js
+++ grails-app/assets/javascripts/spec/lecturerSpec.js
@@ -1,3 +1,20 @@
 /**
  * Created by lars on 28.03.2017.
  */
+describe("Test lecturer.js", function(){
+    beforeEach(function() {
+        //TODO:
+        Document.body.addElement();
+        specRunner.innerHTML +=
+
+    });
+
+    it("Test addQuestion", function() {
+        addQuestion("What is love?");
+        expect(getNewQuestions()).toBe(true);
+        expect(notifyNewQuestion()).toBe(1);
+
+    })
+
+    //TODO: Teardown
+});
