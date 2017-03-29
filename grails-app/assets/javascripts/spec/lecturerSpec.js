badc55f0c0c40f2c647055b0f1fd64bb587a558b
lars møster <lars.moster@hotmail.com>
2017-03-29 21:23:21 +0200

    lecturerSpec added

--- grails-app/assets/javascripts/spec/lecturerSpec.js
+++ grails-app/assets/javascripts/spec/lecturerSpec.js
@@ -3,10 +3,34 @@
  */
 describe("Test lecturer.js", function(){
     beforeEach(function() {
-        //TODO:
-        Document.body.addElement();
-        specRunner.innerHTML +=
+        //INIT
+        var question_container = document.body.createElement("div");
+        question_container.id = "question_container";
+        var default_question = document.body.createElement("div");
+        default_question.id = "default_question";
 
+        //BADGE
+        var badge = document.body.createElement("div");
+        badge.classList.add("new-question-badge");
+
+
+
+        //TODO: denne må de som laget den gjøre
+        var menu_container = document.body.createElement("div");
+        badge.id = "menu-container";
+
+
+    });
+
+
+    it("Test initLecturer", function() {
+
+    });
+
+    it("Test noifyNewQuestion", function() {
+        addQuestion("Why?");
+        expect(notifyNewQuestion().toBe(1));
+        expect(badge.innerHTML = 1);
     });
 
     it("Test addQuestion", function() {
@@ -14,7 +38,35 @@ describe("Test lecturer.js", function(){
         expect(getNewQuestions()).toBe(true);
         expect(notifyNewQuestion()).toBe(1);
 
-    })
+    });
+
+    it ("Test getQuestionsToggled", function() {
+
+    });
+
+    it ("Test resetNewQuestions", function() {
+
+    });
+
+    it ("Test clearAllQuestions", function() {
+
+    });
+
+    it ("Test setDefaultQuestion", function() {
+
+    });
+
+    it ("Test removeDefaultQuestion", function() {
+
+    });
+
+    it ("Test getNewQuestions", function() {
+
+    });
+
+    it ("Test mouseMoveHandler", function() {
+
+    });
 
     //TODO: Teardown
 });
