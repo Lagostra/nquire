bf3822c6d9ce42edb2b5bcf6f281bc60b96c91d0
lars møster <lars.moster@hotmail.com>
2017-04-03 15:42:28 +0200

    init_lecturer test spec

--- grails-app/assets/javascripts/spec/lecturerSpec.js
+++ grails-app/assets/javascripts/spec/lecturerSpec.js
@@ -1,5 +1,7 @@
 /**
  * Created by lars on 28.03.2017.
+ * Time spent 2h - Lars
+ *
  */
 describe("Test lecturer.js", function(){
     beforeEach(function() {
@@ -8,6 +10,8 @@ describe("Test lecturer.js", function(){
         question_container.id = "question_container";
         var default_question = document.body.createElement("div");
         default_question.id = "default_question";
+        var display_question_btn = document.createElement("div");
+        display_question_btn.id = "default_question";
 
         //BADGE
         var badge = document.body.createElement("div");
@@ -24,13 +28,21 @@ describe("Test lecturer.js", function(){
 
 
     it("Test initLecturer", function() {
-        expect(true);
+        initLecturer();
+
+        expect(document.getElementById("question_container")
+            .toBe(question_container));
+        expect(document.getElementById("default_question"))
+            .toBe(default_question);
+        expect(document.getElementById("display_question_btn")
+            .toBe(display_question_btn));
+
     });
 
     it("Test noifyNewQuestion", function() {
         addQuestion("Why?");
         expect(notifyNewQuestion().toBe(1));
-        expect(badge.innerHTML = 1);
+        expect(badge.innerHTML.toBe(1));
     });
 
     it("Test addQuestion", function() {
